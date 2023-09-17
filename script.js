33f0a7422c99e965317e109d9f3c44263e8f707d
unknown <bhushanjoshi1714@gmail.com>
2023-09-17 19:58:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+// link2.addEventListener('click', () => {
+//     // Scroll to the second element with "header" class
+//     scrollToElement('.header', 1);
+// });
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    }
+});
+
+// Add this JavaScript code after the button element is defined in your HTML
+
+var x = document.getElementById("up");
+
+x.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
+
+var a = document.querySelector("#hire");
+
+a.addEventListener('click', () => {
+    window.scrollTo(0, document.body.scrollHeight);
+});
